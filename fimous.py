250094c2d51994eafb57b9c3dec5c21db0f26dea
Lincoln Amorim <54185019+lolincoln@users.noreply.github.com>
2021-07-23 02:22:16 -0400

    Update fimous.py

--- fimous.py
+++ fimous.py
@@ -4,8 +4,8 @@ import time
 #python -m setup.py
 
 
-auth = tweepy.OAuthHandler('4nNJV75ZLujBrE3SDR6jYi3dH','S1vQa7zqg5ePqMpQVdLXdwewUJX5dGjsmrAONIkwuDhXlOJrJg')
-auth.set_access_token('1267610070566699015-xpcj2UAHHHzH7aB2MeJPlUw0aChJjn','T8pysfjucC6m53P9hJ63MYCD9Fyn58Xe2lYzYwQYEjLkk')
+auth = tweepy.OAuthHandler('token','token')
+auth.set_access_token('token-token','token')
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
